00c1866a0b364c0c5f71e4acec8f49055a23b157
fakeITDevTeam <dev5544455@gmail.com>
2023-08-21 13:59:19 +0700

    adding remove item from cartitem

--- ShopOnline.Api/Repositories/ShoppingCartRepository.cs
+++ ShopOnline.Api/Repositories/ShoppingCartRepository.cs
@@ -46,9 +46,18 @@ namespace ShopOnline.Api.Repositories
             return null;
         }
 
-        public Task<CartItem> DeleteItem(int id)
+        public async Task<CartItem> DeleteItem(int id)
         {
-            throw new NotImplementedException();
+            var item = await this.shopOnlineDbContext.CartItems.FindAsync(id);
+
+            if (item != null)
+            {
+                this.shopOnlineDbContext.CartItems.Remove(item);
+
+                await this.shopOnlineDbContext.SaveChangesAsync();
+            }
+
+            return item;
         }
 
         public async Task<CartItem> GetItem(int id)
